655d46efdb7c22dc57a93fbe59a0da6bbe100e84
rodaveli <rodrigoabreu56@gmail.com>
2024-08-23 14:20:56 -0500

    aaa

--- app/session/[id]/page.js
+++ app/session/[id]/page.js
@@ -1,7 +1,6 @@
 "use client"; 
 
 import { useState, useEffect } from 'react';
-import { useState, useEffect } from 'react';
 import { usePathname, useSearchParams } from 'next/navigation';
 import { getSession, submitArgument, getJudgement, submitAppeal, inviteUser, updateUsername } from '../../../../utils/api';
 import ArgumentForm from '../../../../../components/ArgumentForm';
